5e568258ca3c7313b017072333968d5078b4671c
kotori <a572431863@gamil.com>
2023-12-01 13:13:17 +0800

    格式化时间yyyy-MM-dd

--- src/main/java/com/woniuxy/uniformrental/entity/CarYearlyInspection.java
+++ src/main/java/com/woniuxy/uniformrental/entity/CarYearlyInspection.java
@@ -2,6 +2,8 @@ package com.woniuxy.uniformrental.entity;
 
 import java.util.Date;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.io.Serializable;
 
 /**
@@ -14,21 +16,22 @@ import java.io.Serializable;
 public class CarYearlyInspection extends Model<CarYearlyInspection> {
 
     private Long id;
-//年检日期
+    //年检日期
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date maintenanceDate;
-//运营里程数
+    //运营里程数
     private String operatingMileage;
-//年检标识名称
+    //年检标识名称
     private String yearlyInspectionIdentificationName;
-//年检标识
+    //年检标识
     private String yearlyInspectionIdentification;
-//年检状态 2:年检中1:已年检 0:未年检
+    //年检状态 2:年检中1:已年检 0:未年检
     private Long maintenanceState;
-//年检提醒 1已开启 0未开启
+    //年检提醒 1已开启 0未开启
     private String yearlyInspectionReminder;
-//备注
+    //备注
     private String remark;
-//外键
+    //外键
     private Long carId;
 
 
